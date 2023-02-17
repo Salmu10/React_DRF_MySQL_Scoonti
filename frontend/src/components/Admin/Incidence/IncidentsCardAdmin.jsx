d8eee25d6d77065baed314f66106a6f09992326d
Salmu10 <Ubedasalmu@gmail.com>
2023-02-17 18:35:25 +0100

    popover

--- frontend/src/components/Admin/Incidence/IncidentsCardAdmin.jsx
+++ frontend/src/components/Admin/Incidence/IncidentsCardAdmin.jsx
@@ -1,9 +1,12 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './IncidentsCardAdmin.scss';
+import { Popover } from '@headlessui/react'
 
 export default function IncidentsCardAdmin ({ incidence, index, deleteIncidence, type }) {
+    const [filter, setFilter] = useState(null);
+    const [filterData, setFilterData] = useState([]);
 
-    const id = type == 'slot' ? incidence.slot_id : incidence.scooter_id
+    const id = type == 'slot' ? incidence.slot_id : incidence.scooter_id;
 
     return (
         <tr>
@@ -13,7 +16,16 @@ export default function IncidentsCardAdmin ({ incidence, index, deleteIncidence,
             <td>{incidence.desc}</td>
             <td>{id}</td>
             <td>{incidence.user_id}</td>
-            <td> 
+            <td>
+                <Popover className="popover">
+                    <Popover.Button>Update Status</Popover.Button>
+                    <Popover.Panel className="absolute z-10">
+                        <div className="grid grid-cols-2">
+
+                        </div>
+                    </Popover.Panel>
+                </Popover>
+                <button className="buttons" onClick={() => setShow(!show)}>Update</button>
                 <button className="buttons" onClick={() => deleteIncidence(type, incidence.id)}>Delete</button>
             </td>
         </tr>
